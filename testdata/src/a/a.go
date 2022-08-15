920f41f82cffe2e71a3f1627587a6564240afa4e
Tianpeng Wang <tpwang@alauda.io>
2022-08-15 20:08:13 +0800

    chore: Check full vendored path for modules
    Signed-off-by: Tianpeng Wang <tpwang@alauda.io>

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,6 +1,7 @@
 package a
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/go-logr/logr"
@@ -9,7 +10,14 @@ import (
 func Example() {
 	log := logr.Discard()
 	log = log.WithValues("key")                                         // want `odd number of arguments passed as key-value pairs for logging`
-	log.Info("messsage", "key1", "value1", "key2", "value2", "key3")    // want `odd number of arguments passed as key-value pairs for logging`
+	log.Info("message", "key1", "value1", "key2", "value2", "key3")     // want `odd number of arguments passed as key-value pairs for logging`
 	log.Error(fmt.Errorf("error"), "message", "key1", "value1", "key2") // want `odd number of arguments passed as key-value pairs for logging`
 	log.Error(fmt.Errorf("error"), "message", "key1", "value1", "key2", "value2")
+
+	var log2 logr.Logger
+	log2 = log
+	log2.Info("message", "key1") // want `odd number of arguments passed as key-value pairs for logging`
+
+	log3 := logr.FromContextOrDiscard(context.TODO())
+	log3.Error(fmt.Errorf("error"), "message", "key1") // want `odd number of arguments passed as key-value pairs for logging`
 }
