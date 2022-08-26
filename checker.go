a05b1e1c1d3997e199c7e06c87657373a09c8e81
Timon Wong <timon86.wang@gmail.com>
2022-08-26 21:40:07 +0800

    chore: fix ci
    Signed-off-by: Timon Wong <timon86.wang@gmail.com>

--- checker.go
+++ checker.go
@@ -2,42 +2,39 @@ package loggercheck
 
 import "sort"
 
-var (
-	defaultEnabledCheckers = []string{"logr", "klog", "zap"}
-	loggerCheckersByName   = loggerCheckerMap{
-		"logr": {
-			packageImport: "github.com/go-logr/logr",
-			funcs: newStringSet(
-				"(github.com/go-logr/logr.Logger).Error",
-				"(github.com/go-logr/logr.Logger).Info",
-				"(github.com/go-logr/logr.Logger).WithValues"),
-		},
-		"klog": {
-			packageImport: "k8s.io/klog/v2",
-			funcs: newStringSet(
-				"k8s.io/klog/v2.InfoS",
-				"k8s.io/klog/v2.InfoSDepth",
-				"k8s.io/klog/v2.ErrorS",
-				"(k8s.io/klog/v2.Verbose).InfoS",
-				"(k8s.io/klog/v2.Verbose).InfoSDepth",
-				"(k8s.io/klog/v2.Verbose).ErrorS",
-			),
-		},
-		"zap": {
-			packageImport: "go.uber.org/zap",
-			funcs: newStringSet(
-				"(*go.uber.org/zap.SugaredLogger).With",
-				"(*go.uber.org/zap.SugaredLogger).Debugw",
-				"(*go.uber.org/zap.SugaredLogger).Infow",
-				"(*go.uber.org/zap.SugaredLogger).Warnw",
-				"(*go.uber.org/zap.SugaredLogger).Errorw",
-				"(*go.uber.org/zap.SugaredLogger).DPanicw",
-				"(*go.uber.org/zap.SugaredLogger).Panicw",
-				"(*go.uber.org/zap.SugaredLogger).Fatalw",
-			),
-		},
-	}
-)
+var loggerCheckersByName = loggerCheckerMap{
+	"logr": {
+		packageImport: "github.com/go-logr/logr",
+		funcs: newStringSet(
+			"(github.com/go-logr/logr.Logger).Error",
+			"(github.com/go-logr/logr.Logger).Info",
+			"(github.com/go-logr/logr.Logger).WithValues"),
+	},
+	"klog": {
+		packageImport: "k8s.io/klog/v2",
+		funcs: newStringSet(
+			"k8s.io/klog/v2.InfoS",
+			"k8s.io/klog/v2.InfoSDepth",
+			"k8s.io/klog/v2.ErrorS",
+			"(k8s.io/klog/v2.Verbose).InfoS",
+			"(k8s.io/klog/v2.Verbose).InfoSDepth",
+			"(k8s.io/klog/v2.Verbose).ErrorS",
+		),
+	},
+	"zap": {
+		packageImport: "go.uber.org/zap",
+		funcs: newStringSet(
+			"(*go.uber.org/zap.SugaredLogger).With",
+			"(*go.uber.org/zap.SugaredLogger).Debugw",
+			"(*go.uber.org/zap.SugaredLogger).Infow",
+			"(*go.uber.org/zap.SugaredLogger).Warnw",
+			"(*go.uber.org/zap.SugaredLogger).Errorw",
+			"(*go.uber.org/zap.SugaredLogger).DPanicw",
+			"(*go.uber.org/zap.SugaredLogger).Panicw",
+			"(*go.uber.org/zap.SugaredLogger).Fatalw",
+		),
+	},
+}
 
 type loggerChecker struct {
 	packageImport string
